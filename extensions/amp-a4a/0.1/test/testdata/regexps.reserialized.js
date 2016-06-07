fc016c389df8576c89718630c679ae191e1f21b7
Bob Cassels <bcassels@google.com>
2016-06-06 22:06:33 -0400

    Changes to implement a4a (AMP ads for AMP pages).

---
+++ extensions/amp-a4a/0.1/test/testdata/regexps.reserialized.js
@@ -0,0 +1,40 @@
+export const data = `
+<!doctype html>
+<html ⚡>
+<head>
+  <meta charset="utf-8">
+  <link rel="canonical" href="./regular-html-version.html">
+  <meta name="viewport" content="width=device-width,minimum-scale=1">
+  <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
+  <script async src="https://cdn.ampproject.org/v0.js"></script>
+  <script async custom-element="amp-vine" src="https://cdn.ampproject.org/v0/amp-vine-latest.js"></script>
+  <script async custom-element="amp-audio" src="https://cdn.ampproject.org/v0/amp-audio-0.1.js"></script>
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?foobar">
+  <link rel="accessibility">
+  <link rel="accessibility alternate">
+  <link rel="accessibility alternate archives">
+  <meta name="valid" content="">
+  <meta name="validcontent-disposition" content="">
+
+</head>
+<body>
+
+  <amp-audio src="https://example.com/audio" layout="fixed" autoplay="">
+  <amp-audio src="https://example.com/audio" layout="fixed" autoplay="desktop">
+
+
+
+
+</amp-audio></amp-audio><script type="application/json" amp-ad-metadata>
+{
+   "bodyAttributes" : "",
+   "bodyUtf16CharOffsets" : [ 1537, 1720 ],
+   "customElementExtensions" : [ "amp-vine", "amp-audio" ],
+   "customStylesheets" : [
+      {
+         "href" : "https://fonts.googleapis.com/css?foobar"
+      }
+   ]
+}
+</script>
+</body></html>`
