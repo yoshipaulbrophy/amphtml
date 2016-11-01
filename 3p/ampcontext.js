d8e05f41275b359560d49cfac48c2076d4c461aa
Brad Frizzell <bradleyfrizzell@gmail.com>
2016-11-01 09:26:12 -0400

    Implemented unit tests and separated AmpContext.
    Wrote the unit tests for all of the main functionality of
    the AmpContext class. To make this possible, the AmpContext
    class had to be split it into its own file separate from the
    library that binds an instance to window.context.

--- 3p/ampcontext.js
+++ 3p/ampcontext.js
@@ -9,190 +9,181 @@ console.log("AMPCONTEXT.JS");
  *  assign it to window.context, to provide the creative with all the required
  *  functionality.
  */
-window.context = window.context || (function() {
-  // end private stuff in _
-  // const all this stuff
-  const MessageType_ = {
-    SEND_EMBED_STATE: 'send-embed-state',
-    EMBED_STATE: 'embed-state',
-    SEND_EMBED_CONTEXT: 'send-embed-context',
-    EMBED_CONTEXT: 'embed-context',
-    SEND_INTERSECTIONS: 'send-intersections',
-    INTERSECTION: 'intersection',
-    EMBED_SIZE: 'embed-size',
-    EMBED_SIZE_CHANGED: 'embed-size-changed',
-    EMBED_SIZE_DENIED: 'embed-size-denied',
-  };
+export const MessageType_ = {
+  SEND_EMBED_STATE: 'send-embed-state',
+  EMBED_STATE: 'embed-state',
+  SEND_EMBED_CONTEXT: 'send-embed-context',
+  EMBED_CONTEXT: 'embed-context',
+  SEND_INTERSECTIONS: 'send-intersections',
+  INTERSECTION: 'intersection',
+  EMBED_SIZE: 'embed-size',
+  EMBED_SIZE_CHANGED: 'embed-size-changed',
+  EMBED_SIZE_DENIED: 'embed-size-denied',
+};
 
-  //const windowContextCreated = new Event('windowContextCreated');
+//const windowContextCreated = new Event('windowContextCreated');
 
-  class AmpContext {
-    constructor() {
-      /** Map messageType keys to callback functions for when we receive
-       *  that message
-       *  @private {object}
-       */
-      this.callbackFor_ = {};
-      this.setupMetadata_();
-      // Do we want to pass sentinel via hash?  or name attribute?
-      const hashMatch = this.sentinel.match(/((\d+)-\d+)/);
-      if (hashMatch) {
-        // Sentinel has the format of "$windowDepth-$randomNumber".
-        // Depth is measured from window.top.
-        this.depth = Number(hashMatch[2]);
-        this.ancestors = [];
-        for (let win = window; win && win != win.parent; win = win.parent) {
-          // Add window keeping the top-most one at the front.
-          this.ancestors.unshift(win.parent);
-        }
-        this.ampWindow = this.ancestors[this.depth];
-      } else {
-        user().error('Hash does not match amp3pSentinel format');
+export class AmpContext {
+  constructor(win) {
+    /** @private {!Window} */
+    this.win_ = win;
+    /** Map messageType keys to callback functions for when we receive
+     *  that message
+     *  @private {object}
+     */
+    this.callbackFor_ = {};
+    this.setupMetadata_();
+    // Do we want to pass sentinel via hash?  or name attribute?
+    const hashMatch = this.sentinel.match(/((\d+)-\d+)/);
+    if (hashMatch) {
+      // Sentinel has the format of "$windowDepth-$randomNumber".
+      // Depth is measured from window.top.
+      this.depth = Number(hashMatch[2]);
+      this.ancestors = [];
+      for (let win = this.win_; win && win != win.parent; win = win.parent) {
+        // Add window keeping the top-most one at the front.
+        this.ancestors.unshift(win.parent);
       }
-      this.setupEventListener_();
-      //window.dispatchEvent(windowContextCreated);
+      this.ampWindow = this.ancestors[this.depth];
+    } else {
+      user().error('Hash does not match amp3pSentinel format');
     }
+    this.setupEventListener_();
+    //window.dispatchEvent(windowContextCreated);
+  }
 
-    /**
-     *  Request all of the metadata attributes for context and add them to
-     *  the class.
-     *  @private
-     */
-    setupMetadata_() {
-      const data = JSON.parse(window.name);
-      const context = data._context;
-      this.location = context.location;
-      this.canonicalUrl = context.canonicalUrl;
-      this.clientId = context.clientId;
-      this.pageViewId = context.pageViewId;
-      this.sentinel = context.sentinel;
-      this.startTime = context.startTime;
-      this.referrer = context.referrer;
-    }
+  /**
+   *  Request all of the metadata attributes for context and add them to
+   *  the class.
+   *  @private
+   */
+  setupMetadata_() {
+    const data = JSON.parse(this.win_.name);
+    const context = data._context;
+    this.location = context.location;
+    this.canonicalUrl = context.canonicalUrl;
+    this.clientId = context.clientId;
+    this.pageViewId = context.pageViewId;
+    this.sentinel = context.sentinel;
+    this.startTime = context.startTime;
+    this.referrer = context.referrer;
+  }
 
-    /**
-     *
-     */
-    registerCallback_(messageType, callback) {
-      // implicitly this causes previous callback to be dropped!
-      // Should it be an array?  See what current window.context does
-      this.callbackFor_[messageType] = callback;
-      return () => { delete this.callbackFor_[messageType]; };
-    }
+  /**
+   *
+   */
+  registerCallback_(messageType, callback) {
+    // implicitly this causes previous callback to be dropped!
+    // Should it be an array?  See what current window.context does
+    this.callbackFor_[messageType] = callback;
+    return () => { delete this.callbackFor_[messageType]; };
+  }
 
-    /**
-     * Sets up event listener for post messages of the desired type.
-     *   The actual implementation only uses a single event listener for all of
-     *   the different messages, and simply diverts the message to be handled
-     *   by different callbacks.
-     * @private
-     */
-    setupEventListener_() {
-      listen(window, 'message', message => {
-        // Does it look a message from AMP?
-        if (message.source == this.ampWindow && message.data &&
-            message.data.indexOf('amp-') == 0) {
-          // See if we can parse the payload.
-          try {
-            const payload = JSON.parse(message.data.substring(4));
-            // Check the sentinel as well.
-            if (payload.sentinel == this.sentinel &&
-		this.callbackFor_[payload.type]) {
-              try {
-                // We should probably report exceptions within callback
-                this.callbackFor_[payload.type](payload);
-              } catch (err) {
-                user().error(`Error in registered callback ${payload.type}`,
-			err);
-              }
+  /**
+   * Sets up event listener for post messages of the desired type.
+   *   The actual implementation only uses a single event listener for all of
+   *   the different messages, and simply diverts the message to be handled
+   *   by different callbacks.
+   * @private
+   */
+  setupEventListener_() {
+    listen(this.win_, 'message', message => {
+      // Does it look a message from AMP?
+      if (message.source == this.ampWindow && message.data &&
+          message.data.indexOf('amp-') == 0) {
+        // See if we can parse the payload.
+        try {
+          const payload = JSON.parse(message.data.substring(4));
+          // Check the sentinel as well.
+          if (payload.sentinel == this.sentinel &&
+              this.callbackFor_[payload.type]) {
+            try {
+              // We should probably report exceptions within callback
+              this.callbackFor_[payload.type](payload);
+            } catch (err) {
+              user().error(`Error in registered callback ${payload.type}`, err);
             }
-          } catch (e) {
-            // JSON parsing failed. Ignore the message.
           }
+        } catch (e) {
+          // JSON parsing failed. Ignore the message.
         }
-      });
-    };
+      }
+    });
   };
+};
 
-  /**
-   *  Send message to runtime to start sending page visibility messages.
-   *  @param {function} callback Function to call every time we receive a
-   *    page visibility message.
-   *  @returns {function} that when called stops triggering the callback
-   *    every time we receive a page visibility message.
-   */
-  AmpContext.prototype.observePageVisibility = function(callback) {
-    const stopObserveFunc = this.registerCallback_(MessageType_.EMBED_STATE,
+/**
+ *  Send message to runtime to start sending page visibility messages.
+ *  @param {function} callback Function to call every time we receive a
+ *    page visibility message.
+ *  @returns {function} that when called stops triggering the callback
+ *    every time we receive a page visibility message.
+ */
+AmpContext.prototype.observePageVisibility = function(callback) {
+  const stopObserveFunc = this.registerCallback_(MessageType_.EMBED_STATE,
 						 callback);
-    this.ampWindow.postMessage({
-      sentinel: this.sentinel,
-      type: MessageType_.SEND_EMBED_STATE,
-    }, '*');
+  this.ampWindow.postMessage({
+    sentinel: this.sentinel,
+    type: MessageType_.SEND_EMBED_STATE,
+  }, '*');
 
-    return stopObserveFunc;
-  };
+  return stopObserveFunc;
+};
 
-  /**
-   *  Send message to runtime to start sending intersection messages.
-   *  @param {function} callback Function to call every time we receive an
-   *    intersection message.
-   *  @returns {function} that when called stops triggering the callback
-   *    every time we receive an intersection message.
+/**
+ *  Send message to runtime to start sending intersection messages.
+ *  @param {function} callback Function to call every time we receive an
+ *    intersection message.
+ *  @returns {function} that when called stops triggering the callback
+ *    every time we receive an intersection message.
 
-   */
-  AmpContext.prototype.observeIntersection = function(callback) {
-    const stopObserveFunc = this.registerCallback_(MessageType_.INTERSECTION,
+ */
+AmpContext.prototype.observeIntersection = function(callback) {
+  const stopObserveFunc = this.registerCallback_(MessageType_.INTERSECTION,
 						 callback);
-    this.ampWindow.postMessage({
-      sentinel: this.sentinel,
-      type: MessageType_.SEND_INTERSECTIONS,
-    }, '*');
+  this.ampWindow.postMessage({
+    sentinel: this.sentinel,
+    type: MessageType_.SEND_INTERSECTIONS,
+  }, '*');
 
-    return stopObserveFunc;
-  };
+  return stopObserveFunc;
+};
 
-  /**
-   *  Send message to runtime requesting to resize ad to height and width.
-   *    This is not guaranteed to succeed. All this does is make the request.
-   *  @param {int} height The new height for the ad we are requesting.
-   *  @param {int} width The new width for the ad we are requesting.
-   */
-  AmpContext.prototype.requestResize = function(height, width) {
-    this.ampWindow.postMessage({
-      sentinel: this.sentinel,
-      type: MessageType_.EMBED_SIZE,
-      width,
-      height,
-    }, '*');
-  };
+/**
+ *  Send message to runtime requesting to resize ad to height and width.
+ *    This is not guaranteed to succeed. All this does is make the request.
+ *  @param {int} height The new height for the ad we are requesting.
+ *  @param {int} width The new width for the ad we are requesting.
+ */
+AmpContext.prototype.requestResize = function(height, width) {
+  this.ampWindow.postMessage({
+    sentinel: this.sentinel,
+    type: MessageType_.EMBED_SIZE,
+    width,
+    height,
+  }, '*');
+};
 
-  /**
-   *  Allows a creative to set the callback function for when the resize
-   *    request returns a success. The callback should be set before resizeAd
-   *    is ever called.
-   *  @param {function(requestedHeight, requestedWidth)} callback Function
-   *    to call if the resize request succeeds.
-   */
-  AmpContext.prototype.onResizeSuccess = function(callback) {
-    this.registerCallback_(MessageType_.EMBED_SIZE_CHANGED, function(obj) {
-      callback(obj.requestedHeight, obj.requestedWidth); });
-  };
+/**
+ *  Allows a creative to set the callback function for when the resize
+ *    request returns a success. The callback should be set before resizeAd
+ *    is ever called.
+ *  @param {function(requestedHeight, requestedWidth)} callback Function
+ *    to call if the resize request succeeds.
+ */
+AmpContext.prototype.onResizeSuccess = function(callback) {
+  this.registerCallback_(MessageType_.EMBED_SIZE_CHANGED, function(obj) {
+    callback(obj.requestedHeight, obj.requestedWidth); });
+};
 
-  /**
-   *  Allows a creative to set the callback function for when the resize
-   *    request is denied. The callback should be set before resizeAd
-   *    is ever called.
-   *  @param {function(requestedHeight, requestedWidth)} callback Function
-   *    to call if the resize request is denied.
-   */
-  AmpContext.prototype.onResizeDenied = function(callback) {
-    this.registerCallback_(MessageType_.EMBED_SIZE_DENIED, function(obj) {
-      callback(obj.requestedHeight, obj.requestedWidth); });
-  };
-
-
-  return new AmpContext();
-})();
-
-const windowContextCreated = new Event('windowContextCreated');
-window.dispatchEvent(windowContextCreated);
+/**
+ *  Allows a creative to set the callback function for when the resize
+ *    request is denied. The callback should be set before resizeAd
+ *    is ever called.
+ *  @param {function(requestedHeight, requestedWidth)} callback Function
+ *    to call if the resize request is denied.
+ */
+AmpContext.prototype.onResizeDenied = function(callback) {
+  this.registerCallback_(MessageType_.EMBED_SIZE_DENIED, function(obj) {
+    callback(obj.requestedHeight, obj.requestedWidth); });
+};
